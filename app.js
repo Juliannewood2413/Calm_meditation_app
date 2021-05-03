7e04d022eec59a1421331d6fe17fee4f599105c8
Julianne Wood <juliannewood2413@gmail.com>
2021-05-03 15:15:08 -0600

    added js functionality

---
+++ app.js
@@ -0,0 +1,52 @@
+// shout out to devEd for the awesome walk through//
+
+const app = () => {
+    //Elements
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline=  document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    //Get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+
+    //Duration
+    let fakeDuration = 600;
+
+    //get stroke dash array and offset
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    //play sounds
+    play.addEventListener('click', () => {
+        song.play();
+        checkPlay(song);
+    });
+
+    //create function specific to stop and play
+    const checkPlay = song => {
+        if(song.paused){
+            song.play();
+            video.play();
+            play.src= './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src= "./svg/play.svg";
+        }
+
+    }
+
+
+
+
+}
+
+
+app();
